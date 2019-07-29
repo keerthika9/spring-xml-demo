5df925fdeabe9d143e1e7aeee85997960e926f7b
keerthika <atmakurikeerthika@gmail.com>
2019-07-29 07:48:11 +0530

    adding 5th task

--- src/main/java/com/stackroute/domain/Main.java
+++ src/main/java/com/stackroute/domain/Main.java
@@ -1,19 +1,12 @@
 package com.stackroute.domain;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
-
-
-public class Main {
-  public static void main(String[] args){
-    XmlBeanFactory xmlBeanFactory= new XmlBeanFactory ( new ClassPathResource("beans.xml"));
-    Movie movie1= (Movie)xmlBeanFactory.getBean("movie");
-
+public class Main
+{
+  public static void main(String[] args) {
     ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-    Movie movie=(Movie) context.getBean("movie");
-    System.out.println(movie.getActor());
-    System.out.println(movie1.getActor());
+    ((ClassPathXmlApplicationContext) context).registerShutdownHook();
+    BeanLifeCycle beanLifeCycle= (BeanLifeCycle)context.getBean("BeanLifeCycle");
 
   }
 }
